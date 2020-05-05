87c9536e71586ff7942c028c1a5b38e76b04c770
bhavishyachandra <bhavishyachandra@gmail.com>
2020-05-05 03:04:24 -0400

    added cart service

---
+++ src/app/cart.service.ts
@@ -0,0 +1,16 @@
+export class CartService {
+  items = [];
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+}
